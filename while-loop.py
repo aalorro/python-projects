0b270ade7bbedce3b2fe8a47dd5144705a7fa61f
aalorro <aalorro@gmail.com>
2020-05-17 13:53:33 -0400

    added lines and title

--- while-loop.py
+++ while-loop.py
@@ -1,6 +1,8 @@
+#! /usr/bin/python3
 # Python program using 'while loop' to calculate sum of integers
-
+print()
 print("This python program calculates the sum of all integers between 0 and the number you input")
+print()
 
 num = int(input("Enter a positive integer: "))
 init_num = num
@@ -11,5 +13,5 @@ while  num > 0:
     num -= 1
 
 print(str(init_num) + " is the initial number")
-print(str(sum) + " is the sum of all integers inside " + str(init_num))
+print(str(sum) + " is the sum of all integers between 0 and " + str(init_num))
 
